72d0ac7e37ab85273b73f0e9c5911642b24fafc6
VenusBridget <venusbri27@gmail.com>
2024-05-30 18:53:25 +0300

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -2,3 +2,13 @@
 
 def print_fibonacci(length):
     pass
+    seq = []
+    if length > 0:
+        seq.append(0)
+    if length >= 2:
+        seq.append(1)
+        for i in range(2, length):
+            seq.append(seq[-1] + seq[-2])
+            
+    print(seq)
+print_fibonacci(9)
